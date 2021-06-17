45db57d92f59e300ace157123cf72c433f10807f
Ben Shabowski <benshabowski@gmail.com>
2021-06-17 12:59:37 -0500

    Adding a small tutorial for Spring Boot Web
    This is a very simple webstie. Theres some comments in there to read
    about how it works.

---
+++ src/test/java/com/example/Web/MVC/content/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.Web.MVC.content;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+	/*
+	 * This will serve a user a webpage when they go to <website>/greeting
+	 * You can reach this website at: localhost:8080/greeting
+	 * Note: this method also takes an optional argument (name) in the URL with a defualt value or World
+	 * 		This means that if the webpage was reached with a name variable, it will serve something different
+	 * 		example: <webstie>/greeting?name=Ben will say Hello, Ben! instead of Hello, World!
+	 */
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}

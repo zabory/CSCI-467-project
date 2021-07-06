d3795205143cc2d94d434f7f6edaaffd03a17c8c
Ben Shabowski <benshabowski@gmail.com>
2021-07-06 11:39:33 -0500

    Updating DatabaseInterfacer class with an outline

--- src/main/java/Database/DatabaseInterfacer.java
+++ src/main/java/Database/DatabaseInterfacer.java
@@ -1,7 +1,19 @@
 package Database;
 
+import java.sql.Connection;
+
 public class DatabaseInterfacer {
 	
 	public static final String TABLE_NAME = "";
+	
+	private Connection DBConnection;
+	
+	public DatabaseInterfacer () {
+		/*
+		 * TODO Establish database connection
+		 */
+	}
+	
+	// TODO Create methods for interfacing with the database
 
 }

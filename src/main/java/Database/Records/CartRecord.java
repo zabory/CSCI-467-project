119d57f51c200a2f0144a27a58bfc615146c1e2b
Ben Shabowski <benshabowski@gmail.com>
2021-07-14 10:53:42 -0500

    updating cart record

--- src/main/java/Database/Records/CartRecord.java
+++ src/main/java/Database/Records/CartRecord.java
@@ -1,10 +1,16 @@
 package Database.Records;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class CartRecord {
 
 	private String CID;
 	private HashMap<ProductRecord, Integer> products;
+	
+	public CartRecord(ResultSet rs) throws SQLException {
+		
+	}
 	
 }

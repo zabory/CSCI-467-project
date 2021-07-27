f8eb3a53c1524043469187fdc37a77a6cb8f35d3
Ben Shabowski <benshabowski@gmail.com>
2021-07-27 17:24:31 -0500

    Undoing bad stash

--- src/main/java/Controllers/EditDataController.java
+++ src/main/java/Controllers/EditDataController.java
@@ -4,22 +4,14 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import Controllers.Changer.ReceivingChanger;
 import Controllers.Changer.WarehouseChanger;
 import Database.DatabaseInterfacer;
-<<<<<<< Updated upstream
 import Database.Records.PartRecord;
-import Database.Records.ReceivingChanger;
-=======
->>>>>>> Stashed changes
 import application.App;
 
 
@@ -36,15 +28,9 @@ public class EditDataController {
     }
 
     @PostMapping("/r_home/change")
-<<<<<<< Updated upstream
     public String showPage(@ModelAttribute("countChanger") ReceivingChanger bean, Model model) {
         
-        updateInventory(Integer.parseInt(bean.getProductId()), Integer.parseInt(bean.getNewAmount()));
-=======
-    public String showPageR(@ModelAttribute("countChanger") ReceivingChanger bean, Model model) {
-    	
-        System.out.println("Amount: " + bean.getNewAmount() + " " + bean.getProductId()); //in reality, you'd use a logger instead :)
->>>>>>> Stashed changes
+      updateInventory(Integer.parseInt(bean.getProductId()), Integer.parseInt(bean.getNewAmount()));
 
         model.addAttribute("products", DBInterfacer.getAllPartRecords());
         return "r_home";

6eb4fe7c7e4c8d3cf31f51091c08b37a7461f063
Jaos, Leo <ljaos2@illinois.edu>
2021-07-27 15:46:09 -0500

    Order process functionality added

--- src/main/java/Controllers/WarehousePageController.java
+++ src/main/java/Controllers/WarehousePageController.java
@@ -7,13 +7,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import Database.DatabaseInterfacer;
+import Database.Records.OrderRecord;
+import Database.Records.PartRecord;
 import application.App;
 
 @Controller
 @RequestMapping({"w_home"})
 public class WarehousePageController {
 	private DatabaseInterfacer DBInterfacer;
-
+	
 	@RequestMapping
     public String main(Model model) {
         model.addAttribute("orders", DBInterfacer.getAllOrderRecords());
@@ -22,5 +24,28 @@ public class WarehousePageController {
 	@PostConstruct
 	public void initialize() {
 		DBInterfacer = App.getDatabaseInterfacer();
+	}
+	/**
+	 * Processes order record and updates inventory
+	 * @param Oid Order ID
+	 */
+	public void processOrder(int Oid)
+	{
+		OrderRecord record = DBInterfacer.getOrderRecord(Oid);
+		
+		if(record.getAuthorization() == 1)
+		{
+			// ready to process
+			record.setAuthorization(2);
+			
+			DBInterfacer.update(record);
+			// update inventory in DB
+			for(Integer key: record.getParts().keySet())
+			{
+				PartRecord pRecord = DBInterfacer.getPartRecord(key);
+				pRecord.setQuantity(pRecord.getQuantity() - record.getParts().get(key));
+				DBInterfacer.update(pRecord);
+			}
+		}
 	}
 }

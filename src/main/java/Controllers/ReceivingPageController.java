06bca04bb6febea7035b5a7288326fcd02590fbf
SimSam115 <haffnersam@gmail.com>
2021-07-26 19:11:03 -0500

    - Added receiving desk page. along with a page controller ofc - Got modals to work, now we need to update the info in them with a jquery code - Added jquery for use on the changing of elements in the modal

---
+++ src/main/java/Controllers/ReceivingPageController.java
@@ -0,0 +1,27 @@
+package Controllers;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import Database.DatabaseInterfacer;
+import application.App;
+
+@Controller
+@RequestMapping({"r_home"})
+public class ReceivingPageController {
+	private DatabaseInterfacer DBInterfacer;
+
+
+	@RequestMapping
+    public String main(Model model) {
+        model.addAttribute("products", DBInterfacer.getAllPartRecords());
+        return "r_home";
+    }
+	@PostConstruct
+	public void initialize() {
+		DBInterfacer = App.getDatabaseInterfacer();
+	}
+}

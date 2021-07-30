077bf4852a97dedac86f8e231d7620fd7d80c31d
Ben Shabowski <benshabowski@gmail.com>
2021-07-29 20:51:21 -0500

    Adding thresholds to initial a_home page

--- src/main/java/Controllers/EditDataController.java
+++ src/main/java/Controllers/EditDataController.java
@@ -83,6 +83,8 @@ public class EditDataController {
 		model.addAttribute("orders", DBInterfacer.getAllOrderRecords());
 		model.addAttribute("customers", DBInterfacer.getAllCustomerRecords());
 		model.addAttribute("products", DBInterfacer.getAllPartRecords());
+		model.addAttribute("threshold", AdminPageController.getThreshold());
+        model.addAttribute("cost", AdminPageController.getCost());
 
 		return "a_home";
 	}
